9f0eb964a3e39286cad37fdbb959dc290d9616a8
khoojinghwan <hao.07@hotmail.com>
2018-11-27 12:26:15 -0500

    cleanup

--- server/routes/signup.js
+++ server/routes/signup.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
-
 router.post('/', function(req, res, next) {
+    console.log(req.body);
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var email = req.body.email;
+    // email validation is already done with javascript in the front end.
+    // Might want to consider server side validation as well as javascript can be disabled.
+
 	// Comment out this line:
-    console.log("LOGGED", req.body);
     res.send('respond with a resource');
 });
 
