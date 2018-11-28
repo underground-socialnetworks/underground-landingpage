015c61e89053d8693a429a49611b3cda959f7dd6
khoojinghwan <hao.07@hotmail.com>
2018-11-28 10:36:20 -0500

    cleanup

--- server/app.js
+++ server/app.js
@@ -3,23 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
 var signupRouter = require('./routes/signup');
 
 var app = express();
 
+// Serve static files from the React app
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/signup', signupRouter);
 
 // catch 404 and forward to error handler
